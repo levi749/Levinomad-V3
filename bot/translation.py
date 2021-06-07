a61b3702abad88a449b193f89b692e805fb40574
levi749 <84772173+levi749@users.noreply.github.com>
2021-06-07 15:35:24 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -8,13 +8,13 @@ class Translation(object):
 
 <b>I am a Pro AutoFilterBot V2....😜</b>
 
-<b>Make me an admin for your group and channel then connect me....🎉</b>
+<b>IAM Working For Cinemalokam Join group....🎉</b>
 
-<b>🙂Watch the video below to build an Pro Autofilter bot V2 like me...</b>
+<b>🙂 Anyone can create like me bot u just pay once😜...</b>
 
 <b>Subscribe to the update channel to learn about my updates and activity...</b>
 
-<b>Press /help to know about available commands🤪</b>"""    
+ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ <b><a href="https://t.me/Levinomad_TG">©️ levi </a></b>   
     
     HELP_TEXT = """
 <b>😉How To Use Me!😉?</b>
@@ -46,18 +46,18 @@ class Translation(object):
             
             -> <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
             
-<b><a href="https://t.me/Mo_Tech_YT">© Mo Tech YT</a></b>
+<b><a href="https://t.me/Levinomad_TG">© master</a></b>
 """
     
     ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Pro Auto Filter Bot</b>
 
-<b>>➥👨‍💼Master</b> : <b><a href="https://t.me/AlbertEinstein_TG">👤Albert Einstein</a></b>
+<b>>➥👨‍💼Master</b> : <b><a href="https://t.me/Levinomad_TG">👤IAM here bro</a></b>
 
-<b>➥ 👨‍💻Editor</b> : <b><a href="https://t.me/MRk_YT">👤Mrk YT</a></b>
+<b>➥ 👨‍💻Editor</b> : <b><a href="https://t.me/Levinomad_TG">👤Levi</a></b>
 
 <b>➥ 🗣️Language</b> : <b>Python3<b>
 
 <b>➥ 📚Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
 
-<b>➥ 📖Source Code</b> : <b><a href="https://github.com/MoTechYT/Pro-Auto-Filter-Bot-V2">💥Click Me</a></b>
+<b>➥ 📖Source Code</b> : <b><a href="https://github.com/levi749/Levinomad-V3">💥Click Me</a></b>
 """
